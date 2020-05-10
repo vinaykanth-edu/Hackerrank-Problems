7c4f52a1fbf9b2a817daa1dedf0c69c1530d1a56
vinaykanth-edu <vinaykanth.edu@gmail.com>
2020-05-10 15:15:21 +0530

    staircase

--- staircase.js
+++ staircase.js
@@ -1,9 +1,11 @@
-function staircase(n) {
-    var height = n;
+// // **************************************
+// // Method-1
+// // **************************************
 
+function staircase(n) {
+  var height = n;
   for (var i=0 ; i < height; i++) {
     var row= "";
-
     for (var j=0 ; j < height; j++) {
       if (i + j > height - 2 ) {
         row += "#";
@@ -14,5 +16,44 @@ function staircase(n) {
     console.log(row);
   }
 }
-
 staircase(6)
+
+
+// // **************************************
+// // Method-2
+// // **************************************
+function processData(input) {
+  var n = parseInt(input);
+
+  for(var i = 0; i < n; i++)
+  {
+      var s = "";
+      for(var j = 0; j < n; j++)
+      {
+          if(n - i - 2 < j)
+          {
+              s += "#";
+          }
+          else
+          {
+              s += " ";
+          }
+      }
+      console.log(s);
+  }
+}
+
+processData(6)
+
+// // **************************************
+// // Method-3
+// // **************************************
+
+var staircase = "",
+    n = 6,
+    space = " ",
+    hash = "#";
+for(var i = n ; i > 0 ;i--){
+    staircase+=space.repeat(i-1).concat(hash.repeat(n-(i-1))) + "\n";
+}
+console.log(staircase);
