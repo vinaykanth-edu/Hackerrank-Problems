11e2ff5424c4716de70d56a96dcfae78036a5226
vinaykanth-edu <vinaykanth.edu@gmail.com>
2020-05-28 23:55:28 +0530

    updated

--- equalSumGame.js
+++ equalSumGame.js
@@ -1,3 +1,16 @@
+// You have n (n is always even) currency notes which has a positive integer written on it.
+
+// Your country has n / 2 people. Each person gets two currency notes, each note is given to exactly one person.
+
+// As a king of the country, find the way to distribute currency notes such that the sum of values written on it is equal for each resident of your country.
+
+// Note: It is guaranteed that it is always possible.
+// Input Format
+
+// The first line of the input contains integer n — the number of currency notes. It is guaranteed that n is even.
+
+// The second line contains the sequence of n positive integers a1, a2, ..., an (1 ≤ ai ≤ 100), where ai is equal to the number written on the i-th note.
+
 function processData(input) {
     //Enter your code here
     input = input.split('\n')
