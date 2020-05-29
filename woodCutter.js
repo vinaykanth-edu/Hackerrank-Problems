212440c1bff66a4a535d4261317f2412b9bfc6b9
vinaykanth-edu <vinaykanth.edu@gmail.com>
2020-05-29 19:19:34 +0530

    Wood Cutter

---
+++ woodCutter.js
@@ -0,0 +1,25 @@
+function processData(input) {
+    //Enter your code here
+    var data = input.split("\n");
+    var f = data[0].split(" ");
+    var len = Number(f[0]);
+    var height = Number(f[1]);
+    var arr = data[1].split(" ").map(a=>Number(a)).sort((a,b)=>b-a);
+    var sum = 0;
+    for (var i=0;i<len;i++){
+        var cut = 0;
+        sum = sum + arr[i]
+        cut = sum - arr[i]*(i+1)
+        if (cut >= height){
+            sum = sum-arr[i]
+            break;
+        }
+    }
+    if ((sum-height)>=0){
+        console.log(Math.floor((sum-height)/(i)))
+    }
+    else {
+        console.log(-1)
+    }
+    
+} 
