b2227e24133b287ae942900c25e6ea9510f35823
vinaykanth-edu <vinaykanth.edu@gmail.com>
2020-06-21 23:23:37 +0530

    W XXI Contest

---
+++ divisibleSumPairs.js
@@ -0,0 +1,15 @@
+function divisibleSumPairs(n, k, ar) {
+    var count = 0;
+    for(var i=0; i<n; i++)
+        {
+            for(var j=i+1; j<n; j++)
+                {
+                    if((ar[i]+ar[j])%k == 0)
+                        {
+                            count++
+                        }
+                }
+        }
+    return count
+
+}
